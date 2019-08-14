a3382855a0bd16d467835fac968c6772f3171216
ddyh2901 <40836969+ddyh2901@users.noreply.github.com>
2019-08-14 21:00:43 +0900

    Add files via upload

---
+++ dp/합분해.cpp
@@ -0,0 +1,31 @@
+﻿#pragma warning (disable: 4996)
+
+#include <cstdio>
+
+using namespace std;
+
+long long d[201][201];
+
+int main(void)
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+
+	d[0][0] = 1LL;
+	for (int k = 1; k <= b; k++)
+	{
+		for (int i = 0; i <= a; i++)
+		{
+			for (int j = 0; j <= i; j++)
+			{
+				d[k][i] += d[k - 1][i - j];
+				d[k][i] %= 1000000000;
+			}
+		}
+	}
+
+	printf("%lld", d[b][a]);
+
+	return 0;
+}
