b69c94d330c3d0aa629087e6ff6d066502d10974
ddyh2901 <40836969+ddyh2901@users.noreply.github.com>
2019-08-16 20:06:23 +0900

    Add files via upload

---
+++ 브루트포스/카잉 달력.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+	int c;
+	while (b != 0)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int lcm(int a, int b)
+{
+	return a * b / gcd(a, b);
+}
+
+int main(void)
+{
+	int n;
+	int a, b, c, d;
+
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int count = -1;
+
+		scanf("%d %d %d %d", &a, &b, &c, &d);
+
+		int temp = lcm(a, b);
+
+		for (int j = c; j <= temp; j = j + a)
+		{
+
+			if ((j % b) == d || (j % b) == 0 && b == d)
+			{
+				count = j;
+				break;
+			}
+		}
+		printf("%d", count);
+		if (i != n - 1)
+			printf("\n");
+	}
+
+	return 0;
+}
