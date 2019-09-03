7f56b7361117b05e6bac80641dbfbf00a2c4324f
ddyh2901 <40836969+ddyh2901@users.noreply.github.com>
2019-09-03 21:38:46 +0900

    Add files via upload

---
+++ dp/점프.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a[101][101];
+long long d[101][101];
+
+int main(void)
+{
+	int n;
+	int x, y;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+
+	d[1][1] = 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (a[i][j] == 0)
+				continue;
+			
+			if (i + a[i][j]<= n)
+			{
+				d[i + a[i][j]][j] += d[i][j];
+			}
+			
+			if (j + a[i][j] <= n)
+			{
+				d[i][j + a[i][j]] += d[i][j];
+			}
+		}
+	}
+
+	printf("%lld", d[n][n]);
+	return 0;
+}
