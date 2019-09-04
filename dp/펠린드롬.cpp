0bfcc33dd181c15da5411b7186532ae965e1be2a
ddyh2901 <40836969+ddyh2901@users.noreply.github.com>
2019-09-04 22:37:03 +0900

    Add files via upload

---
+++ dp/펠린드롬.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int a[2001];
+int d[2001][2001];
+
+int go(int x, int y)
+{
+	if (x == y)
+		return 1;
+	if (x + 1 == y)
+	{
+		if (a[x] == a[y])
+			return 1;
+		else
+			return 0;
+	}
+	if (d[x][y] >= 0)
+		return d[x][y];
+	if (a[x] != a[y])
+		return 0;
+	return go(x + 1, y - 1);
+}
+
+int main(void)
+{
+	memset(d, -1, sizeof(d));
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+		scanf("%d", &a[i]);
+
+	int num;
+	scanf("%d", &num);
+
+	int x, y;
+
+	for (int i = 1; i <= num; i++)
+	{
+		scanf("%d %d", &x, &y);
+
+		d[x][y] = go(x, y);
+		printf("%d\n", d[x][y]);
+	}
+	return 0;
+}
