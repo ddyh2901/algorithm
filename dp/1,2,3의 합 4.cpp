474c9e7561db5ef92b42a9341bde14ac8e63f6db
ddyh2901 <40836969+ddyh2901@users.noreply.github.com>
2019-09-08 16:18:18 +0900

    Add files via upload

---
+++ dp/1,2,3의 합 4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+long long d[1000001];
+long long mod = 10009LL;
+int limit = 100000;
+
+int main() {
+	d[0] = 1;
+	for (int i = 1; i <= limit; i++) {
+		if (i - 1 >= 0) {
+			d[i] += d[i - 1];
+		}
+	}
+	for (int i = 1; i <= limit; i++) {
+		if (i - 2 >= 0) {
+			d[i] += d[i - 2];
+		}
+	}
+	for (int i = 1; i <= limit; i++) {
+		if (i - 3 >= 0) {
+			d[i] += d[i - 3];
+		}
+	}
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		scanf("%d", &n);
+		printf("%lld\n", d[n]);
+	}
+}
