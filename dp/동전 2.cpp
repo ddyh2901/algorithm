252d7e31028ffd236421868b49c3c37e16eed7a0
ddyh2901 <40836969+ddyh2901@users.noreply.github.com>
2019-09-10 01:31:47 +0900

    Add files via upload

---
+++ dp/동전 2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int a[101];
+int d[10001];
+
+int main() {
+	int n, sum;
+	scanf("%d %d", &n, &sum);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	memset(d, -1, sizeof(d));
+
+	d[0] = 0;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= sum; j++) {
+			if (j - a[i] >= 0 && d[j - a[i]] != -1) {
+				if (d[j] == -1 || d[j] > d[j - a[i]] + 1) {
+					d[j] = d[j - a[i]] + 1;
+				}
+			}
+		}
+	}
+
+	printf("%d", d[sum]);
+
+	return 0;
+}
