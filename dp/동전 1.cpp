b6b9701f98cf3ac8bc2edd5f029df9cb307718a4
ddyh2901 <40836969+ddyh2901@users.noreply.github.com>
2019-09-09 16:18:34 +0900

    Add files via upload

---
+++ dp/동전 1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long a[101];
+long long d[10001];
+
+int main(void)
+{
+	int n, sum;
+	scanf("%ld %ld", &n, &sum);
+
+	for (int i = 0; i < n; i++)
+		scanf("%ld", &a[i]);
+
+	d[0] = 1;
+
+	for (int j = 0; j < n; j++)
+	{
+		for (int i = 1; i <= sum; i++)
+		{
+			if (i - a[j] >= 0)
+			{
+				d[i] += d[i - a[j]];
+			}
+		}
+	}
+
+	printf("%lld", d[sum]);
+	return 0;
+}
