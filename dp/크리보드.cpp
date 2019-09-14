9045df97f7fac27295a46d6fc36a3f100068fb20
ddyh2901 <40836969+ddyh2901@users.noreply.github.com>
2019-09-14 16:31:50 +0900

    Add files via upload

---
+++ dp/크리보드.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+long long d[101];
+
+int main(void)
+{
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 3;
+
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 4; i <= n; i++)
+	{
+		d[i] = d[i - 1] + 1;
+		for (int j = 1; j <= i - 3; j++)
+		{
+			d[i] = max(d[i], d[i - j - 2] * (j + 1));
+		}
+	}
+
+	printf("%lld", d[n]);
+
+	return 0;
+}
